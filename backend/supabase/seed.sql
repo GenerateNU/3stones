c26beacbaaaed7050e2f041c5444c6d753599725
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2024-09-20 15:40:04 -0400

    More seed data

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -2,3 +2,8 @@ INSERT INTO contributors (first_name, last_name, email) VALUES ('Michael', 'Bren
 INSERT INTO contributors (first_name, last_name, email) VALUES ('Ryan', 'Saperstein', 'saperstein.r@northeastern.edu');
 
 INSERT INTO developers (name, description, location) VALUES ('Developer 1', 'Developer 1 description', 'Developer 1 location');
+INSERT INTO developers(name, description, location) VALUES('William', 'Williams description', 'Boston');
+INSERT INTO developers (name, description, location) VALUES ('Ruby', 'Rubys description', 'Portland');
+INSERT INTO developers (name, description, location) VALUES ('Rachel', 'Rachels description', 'New York');
+INSERT INTO developers (name, description, location) VALUES ('Bob', 'Bobs description', 'LA');
+INSERT INTO developers (name, description, location) VALUES ('Lily', 'Lilys description', 'San Diego');
