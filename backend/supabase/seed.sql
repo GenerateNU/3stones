9e92228cd7c3a02aca4b347efa3d0f7462caf672
gus-s-42 <gusnbastar@gmail.com>
2024-09-21 13:33:58 -0400

    Added temporary seeding data for projects

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -1,2 +1,4 @@
 INSERT INTO contributors (first_name, last_name, email) VALUES ('Michael', 'Brennan', 'brennan.mic@northeastern.edu');
 INSERT INTO contributors (first_name, last_name, email) VALUES ('Ryan', 'Saperstein', 'saperstein.r@northeastern.edu');
+INSERT INTO projects (developer_id, title, location, funding_goal_cents) VALUES ('123123123', "Bowser's Castle", 'The Valley of Bowser', '10000000000');
+INSERT INTO projects (developer_id, title, location, funding_goal_cents) VALUES ('212313543234', "Spongebob's Pineapple", '124 Conch St.', '1800000000');
