1135b284bc2adb4a17eea7bea5c98e64740385b1
Michael Brennan <michael.brennancolorado@gmail.com>
2024-09-23 11:05:16 -0400

    error handling

--- backend/internal/errors/api_error.go
+++ backend/internal/errors/api_error.go
@@ -3,11 +3,7 @@ package errors
 import "github.com/gofiber/fiber/v2"
 
 type ApiError struct {
-	// 200 - Request completed successfully
-	// 400 - Requester messed something up (unauthorized, unknown id, etc.)
-	// 500 - Server messed something up (internal error, etc.)
-	Code int
-	// Formatting should be camel case, i.e "jwt_has_expired","no_user_found",etc.
+	Code    int
 	Message string
 }
 
