1ada87468e40f73c162d23e0155e95a6eb7bae95
gus-s-42 <gusnbastar@gmail.com>
2024-09-24 10:40:33 -0400

    Added developer id reference to project seeds

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -1,4 +1,5 @@
 INSERT INTO contributors (first_name, last_name, email) VALUES ('Michael', 'Brennan', 'brennan.mic@northeastern.edu');
 INSERT INTO contributors (first_name, last_name, email) VALUES ('Ryan', 'Saperstein', 'saperstein.r@northeastern.edu');
-INSERT INTO projects (developer_id, title, location, funding_goal_cents) VALUES ('123123123', "Bowser's Castle", 'The Valley of Bowser', '10000000000');
-INSERT INTO projects (developer_id, title, location, funding_goal_cents) VALUES ('212313543234', "Spongebob's Pineapple", '124 Conch St.', '1800000000');
+INSERT INTO developers (name, description, location) VALUES ('Arav', 'Developer for Generate', 'Boston, MA');
+INSERT INTO projects (developer_id, title, location, funding_goal_cents) VALUES (SELECT id FROM developers WHERE name = "Arav", "Bowser's Castle", 'The Valley of Bowser', '10000000000');
+INSERT INTO projects (developer_id, title, location, funding_goal_cents) VALUES (SELECT id FROM developers WHERE name = "Arav", "Spongebob's Pineapple", '124 Conch St.', '1800000000');
