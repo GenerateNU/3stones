fc6d11b5f795834d01f698c576e5f02ba1aeb40f
gus-s-42 <gusnbastar@gmail.com>
2024-09-24 10:57:24 -0400

    Fixed json error in project.go

--- backend/internal/models/project.go
+++ backend/internal/models/project.go
@@ -6,7 +6,7 @@ type Project struct {
 	ID               uuid.UUID `json:"id"`
 	DeveloperID      uuid.UUID `json:"developer_id"`
 	Title            string    `json:"title"`
-	Description      string    `json:description"`
+	Description      string    `json:"description"`
 	Location         string    `json:"location"`
 	Completed        bool      `json:"completed"`
 	FundingGoalCents int32     `json:"funding_goal_cents"`
