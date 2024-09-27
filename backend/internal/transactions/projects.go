a8f9859e507910c99a81fbc785a4dd32d5a2b0fe
Sumer Shinde <sumershinde22@gmail.com>
2024-09-27 13:58:30 -0400

    completed projects

--- backend/internal/transactions/projects.go
+++ backend/internal/transactions/projects.go
@@ -57,50 +57,17 @@ func GetProjects(db *pgxpool.Pool) ([]models.Project, error) {
 	return projects, nil
 }
 
-func GetProjectTotalFunded(db *pgxpool.Pool, id uuid.UUID) ([]models.Project, error) {
-	rows, err := db.Query(context.Background(), "SELECT SUM(funded_cents) FROM investor_investments JOIN projects ON investor_investments.project_id = projects.id WHERE projects.id = $1", id)
+func GetProjectTotalFunded(db *pgxpool.Pool, id uuid.UUID) (int64, error) {
+	var totalFundedCents int64
+
+	err := db.QueryRow(context.Background(), "SELECT COALESCE(SUM(funded_cents), 0) as sum FROM investor_investments JOIN projects ON investor_investments.project_id = projects.id WHERE projects.id = $1", id).Scan(&totalFundedCents)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 
-	projects := []models.Project{}
-
-	defer rows.Close()
-	for rows.Next() {
-		var id uuid.UUID
-		var developerID uuid.UUID
-		var title string
-		var description string
-		var completed bool
-		var fundingGoalCents int32
-		var premise string
-		var street string
-		var locality string
-		var state string
-		var zipcode string
-
-		err = rows.Scan(&id, &developerID, &title, &description, &completed, &fundingGoalCents, &premise, &street, &locality, &state, &zipcode)
-		if err != nil {
-			return nil, err
-		}
-
-		projects = append(projects, models.Project{
-			ID:               id,
-			DeveloperID:      developerID,
-			Title:            title,
-			Description:      description,
-			Completed:        completed,
-			FundingGoalCents: fundingGoalCents,
-			Premise:          premise,
-			Street:           street,
-			Locality:         locality,
-			State:            state,
-			Zipcode:          zipcode,
-		})
-	}
-
-	return projects, nil
+	return totalFundedCents, nil
 }
+
 
 func GetProjectById(db *pgxpool.Pool, id uuid.UUID) (*models.Project, error) {
 	// Execute the query with the provided context and developer ID
