1dfb472afb6d5ec1cabf30b4a91bb008ea52f72c
gus-s-42 <gusnbastar@gmail.com>
2024-09-28 15:27:24 -0400

    For PR (initial implementation for transactions for portfolio included)

---
+++ backend/internal/transactions/portfolio.go
@@ -0,0 +1,33 @@
+package transactions
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func GetPortfolio(db *pgxpool.Pool, investorID string) (map[string]int, error) {
+	query := "SELECT project_id, SUM(funded_cents) AS total_investment FROM investor_investments WHERE investor_id = $1 GROUP BY project_id"
+
+	rows, err := db.Query(context.Background(), query, investorID)
+	if err != nil {
+		return nil, err
+	}
+
+	var portfolio map[uuid.UUID]int
+	portfolio = make(map[uuid.UUID]int)
+
+	defer rows.Close()
+	for rows.Next() {
+		var projectID uuid.UUID
+		var totalFunded int
+
+		err = rows.Scan(&projectID, &totalFunded)
+		if err != nil {
+			return nil, err
+		}
+		portfolio[projectID] = totalFunded
+	}
+
+	return portfolio, nil
+}
--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -76,3 +76,16 @@ INSERT INTO developers (id, name, description, premise, street, locality, state,
 INSERT INTO projects (id, developer_id, title, description, completed, funding_goal_cents, premise, street, locality, state, zipcode) VALUES ('c3733692-5a86-441f-8ad0-9c32c648bb72', '56ebee48-d844-4fcd-aa58-fb71688c3e81', 'Bowser Castle', 'A big fiery castle sitting on prime real estate', 'FALSE', '100000000', '7', 'Speare Pl', 'Boston', 'MA', '02115');
 INSERT INTO projects (id, developer_id, title, description, completed, funding_goal_cents, premise, street, locality, state, zipcode) VALUES ('d09c8f0f-13d3-4336-92e9-b0b2c8bce570', '56ebee48-d844-4fcd-aa58-fb71688c3e81', 'Spongebob Pineapple', 'A pineapple under the sea', 'TRUE', '18000000', '7', 'Speare Pl', 'Boston', 'MA', '02115');
 
+INSERT INTO investors (supabase_id, first_name, last_name) VALUES ((SELECT id from auth.users where email='user1@example.com'), 'Dwight', 'Howard');
+INSERT INTO investors (supabase_id, first_name, last_name) VALUES ((SELECT id from auth.users where email='user2@example.com'), 'Taj', 'Gibson');
+
+
+INSERT INTO investor_investments (id, project_id, investor_id, funded_cents) VALUES ('bdd66406-64bd-41a5-b797-a486751ea429', 'c3733692-5a86-441f-8ad0-9c32c648bb72', (SELECT id FROM auth.users WHERE email='user1@example.com'), '1200');
+INSERT INTO investor_investments (id, project_id, investor_id, funded_cents) VALUES ('ba995313-1b8a-4b4e-ad02-0b72efd22309', 'c3733692-5a86-441f-8ad0-9c32c648bb72', (SELECT id FROM auth.users WHERE email='user1@example.com'), '400');
+INSERT INTO investor_investments (id, project_id, investor_id, funded_cents) VALUES ('de194a16-c0be-4519-bf6a-9a3bceea1b42', 'c3733692-5a86-441f-8ad0-9c32c648bb72', (SELECT id FROM auth.users WHERE email='user1@example.com'), '800');
+
+INSERT INTO investor_investments (id, project_id, investor_id, funded_cents) VALUES ('a9daf2f3-67c5-4f34-8c7c-0b4079a90c91', 'd09c8f0f-13d3-4336-92e9-b0b2c8bce570', (SELECT id FROM auth.users WHERE email='user1@example.com'), '1200');
+INSERT INTO investor_investments (id, project_id, investor_id, funded_cents) VALUES ('00ae74b8-dac2-47cd-8c79-eb543e0c0b41', 'd09c8f0f-13d3-4336-92e9-b0b2c8bce570', (SELECT id FROM auth.users WHERE email='user1@example.com'), '100');
+
+INSERT INTO investor_investments (id, project_id, investor_id, funded_cents) VALUES ('874c693a-fefb-4a6f-8988-a85cfadb8fe2', 'd09c8f0f-13d3-4336-92e9-b0b2c8bce570', (SELECT id FROM auth.users WHERE email='user2@example.com'), '2000');
+
