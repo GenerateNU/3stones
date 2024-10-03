48e7c8af922a55ed972a42d2eb81020b1e8ebc6b
gus-s-42 <156837430+gus-s-42@users.noreply.github.com>
2024-10-03 10:49:52 -0400

    Scrum 15 portfolio (#30)
    * For PR (initial implementation for transactions for portfolio included)
    
    * Get portfolio implemented
    
    * Deleted empty file
    
    * Formatting
    
    * Formatting with gofumpt
    
    * Making the linter happy
    
    * removed id Param, using Locals instead
    
    * formatting
    
    * deleted old code
    
    * uuid nit
    
    ---------
    
    Co-authored-by: Melody Yu <yu.melo@northeastern.edu>
    Co-authored-by: Michael Brennan <michael.brennancolorado@gmail.com>

---
+++ backend/internal/routes/investors.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"backend/internal/controllers"
+	"backend/internal/types"
+)
+
+func Investors(params types.RouterParams) {
+	investorsController := controllers.NewInvestorsController(params.ServiceParams)
+
+	// api/v1/investors/*
+	investors := params.Router.Group("/investors")
+
+	investors.Get("/portfolio", params.Auth.Middleware(), investorsController.GetPortfolio)
+}
