cf7768b6d2e78c09bfb5864965bf3cd10cbd76d8
Sumer Shinde <sumershinde22@gmail.com>
2024-10-07 18:00:52 -0400

    Added timestamp

--- backend/internal/transactions/history.go
+++ backend/internal/transactions/history.go
@@ -2,13 +2,14 @@ package transactions
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func GetHistory(db *pgxpool.Pool, investorID uuid.UUID, limit int, offset int) ([]map[string]interface{}, error) {
-	query := "SELECT project_id, funded_cents FROM investor_investments WHERE investor_id = $1 ORDER BY current_timestamp LIMIT $2 OFFSET $3"
+	query := "SELECT created_at, project_id, funded_cents FROM investor_investments WHERE investor_id = $1 ORDER BY created_at LIMIT $2 OFFSET $3"
 	rows, err := db.Query(context.Background(), query, investorID, limit, offset)
 	if err != nil {
 		return nil, err
@@ -18,15 +19,17 @@ func GetHistory(db *pgxpool.Pool, investorID uuid.UUID, limit int, offset int) (
 	defer rows.Close()
 
 	for rows.Next() {
+		var createdAt time.Time
 		var projectID uuid.UUID
 		var fundedCents int
 
-		err = rows.Scan(&projectID, &fundedCents)
+		err = rows.Scan(&createdAt, &projectID, &fundedCents)
 		if err != nil {
 			return nil, err
 		}
 
 		entry := map[string]interface{}{
+			"created_at":   createdAt.Format(time.RFC3339),
 			"project_id":   projectID,
 			"funded_cents": fundedCents,
 		}
