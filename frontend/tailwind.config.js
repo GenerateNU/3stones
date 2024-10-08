5cd67a7ea774e9e8d5fc271a01bffc121133058d
Ryan Saperstein <saperstein.r@northeastern.edu>
2024-10-08 17:05:11 -0400

    [SCRUM-22] Add custom colors and fonts to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}', './src/screens/home/home.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000000',
+        white: 'ffffff',
+        defaultPrimary: '#285852',
+        defaultPush: '#65918C',
+        defaultDisabled: '#A7C6C2',
+        defaultText: '#282828',
+        disabledText: '#868686',
+        inverseText: 'F3F3F3',
+        surfaceBG: '#FAFAFC',
+        surfaceFG: '#FFFFFF',
+        border: '#DDDDDD',
+      },
+      fontFamily: {
+        nunitoBlack: ['Nunito-Black'],
+        nunitoBoldItalic: ['Nunito-BoldItalic'],
+        heading: ['Nunito-Bold'],
+        nunitoRegular: ['Nunito-Regular'],
+        title: ['Nunito-ExtraBold'],
+        bodyBold: ['Inter_18pt-Bold'],
+        body: ['Inter_18pt-Regular'],
+      },
+    },
   },
-  plugins: [],
 };
