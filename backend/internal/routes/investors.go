6698628a45819634ff6d1392d24fd4210699f266
Michael Brennan <michael.brennancolorado@gmail.com>
2024-10-09 12:37:19 -0400

    sorry dao - keep investors investors

--- backend/internal/routes/investors.go
+++ backend/internal/routes/investors.go
@@ -9,7 +9,7 @@ func Investors(params types.RouterParams) {
 	investorsController := controllers.NewInvestorsController(params.ServiceParams)
 
 	// api/v1/investors/*
-	investors := params.Router.Group("/investor")
+	investors := params.Router.Group("/investors")
 	investors.Get("/profile", params.Auth.Middleware(), investorsController.GetProfile)
 
 	investors.Get("/portfolio", params.Auth.Middleware(), investorsController.GetPortfolio)
