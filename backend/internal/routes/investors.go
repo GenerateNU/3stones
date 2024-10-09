a8f3f5d45937d78464704b774a780eb5c9cc442e
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2024-10-09 12:34:24 -0400

    Scrum 17 get user profile (#32)
    * Seeding Data for the Investor_Investments Table
    
    * Documentation, better variable names, little fixes
    
    * SCRUM-10: pst endpoint implemented + api error constants
    
    * linting + format + removed debugging console log
    
    * PR comment fixes
    
    * SQL query style edit
    
    * formatting fixes
    
    * removed conflict marker
    
    * added return status codes
    
    ---------
    
    Co-authored-by: abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
    Co-authored-by: Michael Brennan <michael.brennancolorado@gmail.com>

--- backend/internal/routes/investors.go
+++ backend/internal/routes/investors.go
@@ -9,7 +9,9 @@ func Investors(params types.RouterParams) {
 	investorsController := controllers.NewInvestorsController(params.ServiceParams)
 
 	// api/v1/investors/*
-	investors := params.Router.Group("/investors")
+	investors := params.Router.Group("/investor")
+	investors.Get("/profile", params.Auth.Middleware(), investorsController.GetProfile)
 
 	investors.Get("/portfolio", params.Auth.Middleware(), investorsController.GetPortfolio)
+	investors.Get("/", params.Auth.Middleware(), investorsController.GetInvestor)
 }
