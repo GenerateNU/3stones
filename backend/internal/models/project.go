78469c007e89ce5d81b876573efab583f58126af
Michael Brennan <76018881+michael-brennan2005@users.noreply.github.com>
2024-10-15 12:41:00 -0400

    Smorgasborg (#37)
    * Project images
    
    * Project post images
    
    * milestones

--- backend/internal/models/project.go
+++ backend/internal/models/project.go
@@ -7,17 +7,19 @@ import (
 )
 
 type Project struct {
-	ID               uuid.UUID `json:"id"`
-	DeveloperID      uuid.UUID `json:"developer_id"`
-	Title            string    `json:"title"`
-	Description      string    `json:"description"`
-	Completed        bool      `json:"completed"`
-	FundingGoalCents int32     `json:"funding_goal_cents"`
-	Premise          string    `json:"premise"`
-	Street           string    `json:"street"`
-	Locality         string    `json:"locality"`
-	State            string    `json:"state"`
-	Zipcode          string    `json:"zipcode"`
+	ID               uuid.UUID   `json:"id"`
+	DeveloperID      uuid.UUID   `json:"developer_id"`
+	Title            string      `json:"title"`
+	Description      string      `json:"description"`
+	Completed        bool        `json:"completed"`
+	FundingGoalCents int32       `json:"funding_goal_cents"`
+	Milestone        string      `json:"milestone"`
+	Premise          string      `json:"premise"`
+	Street           string      `json:"street"`
+	Locality         string      `json:"locality"`
+	State            string      `json:"state"`
+	Zipcode          string      `json:"zipcode"`
+	Images           []ImageLink `json:"images"`
 }
 
 type InvestRequestBody struct {
@@ -25,9 +27,15 @@ type InvestRequestBody struct {
 }
 
 type ProjectPost struct {
-	ID          uuid.UUID `json:"id"`
-	CreatedAt   time.Time `json:"created_at"`
-	ProjectID   uuid.UUID `json:"project_id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
+	ID          uuid.UUID   `json:"id"`
+	CreatedAt   time.Time   `json:"created_at"`
+	ProjectID   uuid.UUID   `json:"project_id"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	Images      []ImageLink `json:"images"`
+}
+
+type ImageLink struct {
+	ID  uuid.UUID `json:"id"`
+	Url string    `json:"url"`
 }
