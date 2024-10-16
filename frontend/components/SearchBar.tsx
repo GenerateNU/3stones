04479fce98134561e0a24932a01239b96333b5a8
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2024-10-15 20:25:15 -0400

    finished shadow + text inputs

--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -1,24 +1,69 @@
 import React from 'react';
-import { Image, Text, View, TextInput } from 'react-native';
+import { Image, View, TextInput, StyleSheet, Dimensions } from 'react-native';
 import { styled } from 'nativewind';
 
 const StyledView = styled(View);
-// const StyledText = styled(Text);
-const StyledTextInput = styled(TextInput);
 const StyledImage = styled(Image);
 
 const searchIcon = require('../assets/images/search-icon.png');
 
+// Get screen width and height
+const { width, height } = Dimensions.get('window');
+
+// Used styleSheet instead of Native wind due to the drop shadow not rendering on Native wind. 
+// Width and Height were used to determine viewport's width/height (responsive design)
+const styles = StyleSheet.create({
+    shadowSearchBar: {
+        // 75 vw and 5 vh
+        width: width * 0.75,
+
+        height: height * 0.05,
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: height * 0.007,
+        },
+        borderRadius: width * 0.035,
+        shadowOpacity: 0.25,
+        shadowRadius: height * 0.006,
+        //for android, change this
+        elevation: height * 0.05,
+        paddingLeft: width * 0.1,
+        fontSize: width * 0.04,
+        color: 'black',
+        fontFamily: 'Inter'
+    }
+});
+
 
 const searchBar = () => {
-    function onChangeInput() {
-        console.log('triggered');
+    //current text input
+    const [input, setInput] = React.useState('');
+
+    //function that updates input while searching
+    function onChangeInput(text) {
+        setInput(text);
+        console.log('Input: ', text);
     }
+
+    //function that handles search submission
+    function onSubmitInput() {
+        console.log('Submitted: ', input);
+    }
+
     return (
-        <StyledView className="flex justify-center">
-            <StyledImage source={searchIcon} className="absolute w-[4vw] h-[2vh]"/>
-            <StyledTextInput onChangeText={onChangeInput} placeholder="Search" className='w-[75vw] h-[5vh] border-none rounded-r-[3vw] rounded-l-[3vw] placeholder:pl-[6vw] placeholder:text-[5vw]'></StyledTextInput>
-        </StyledView>
+        <StyledView className="flex-inline justify-center">
+            <StyledImage source={searchIcon} className="absolute w-[4vw] h-[2vh] left-[4vw] z-20"/>
+                <TextInput
+                    onChangeText={onChangeInput}
+                    value={input}
+                    onSubmitEditing={onSubmitInput}
+                    placeholder="Search"
+                    placeholderTextColor="#5E5E5E"
+                    style={styles.shadowSearchBar}
+                />
+                </StyledView>
     );
 };
 
