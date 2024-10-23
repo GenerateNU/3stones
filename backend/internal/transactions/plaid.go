8d3095071f952b741a0d150ca74dcff6e6c3e3f0
Sumer Shinde <sumershinde22@gmail.com>
2024-10-23 19:12:30 -0400

    Updated sql update query to update access token and item id

--- backend/internal/transactions/plaid.go
+++ backend/internal/transactions/plaid.go
@@ -9,10 +9,10 @@ import (
 
 func StoreAccessToken(db *pgxpool.Pool, userID uuid.UUID, accessToken, itemID string) error {
 	query := `
-        INSERT INTO investors (access_token, item_id)
-        VALUES ($1, $2)
-        where supabase_id = $3
-    `
+			UPDATE investors
+			SET access_token = '$1', item_id = '$2'
+			WHERE supabase_id = '$3';
+			`
 
 	_, err := db.Exec(context.Background(), query, accessToken, itemID, userID)
 	return err
