fa2e77444b47d57dac47c576517c055338039ae4
Michael Brennan <michael.brennancolorado@gmail.com>
2024-11-01 11:33:16 -0400

    fix auth

--- backend/internal/routes/projects.go
+++ backend/internal/routes/projects.go
@@ -12,8 +12,8 @@ func Projects(params types.RouterParams) {
 	projects := params.Router.Group("/projects")
 
 	projects.Get("/", params.Auth.Middleware(), projectsController.GetProjects)
-	projects.Get("/:id" /* params.Auth.Middleware() ,*/, projectsController.GetProjectById)
-	projects.Get("/:id/total-funded" /* params.Auth.Middleware() ,*/, projectsController.GetProjectTotalFunded)
+	projects.Get("/:id", params.Auth.Middleware(), projectsController.GetProjectById)
+	projects.Get("/:id/total-funded", params.Auth.Middleware(), projectsController.GetProjectTotalFunded)
 	projects.Post("/:id/invest", params.Auth.Middleware(), projectsController.Invest)
 	projects.Get("/:id/posts", params.Auth.Middleware(), projectsController.GetProjectPosts)
 }
