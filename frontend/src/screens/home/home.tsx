4693a800f93bac2a93c6274aaf5b273041b0918c
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2024-11-01 03:50:16 -0400

    finished auth integration

--- frontend/src/screens/home/home.tsx
+++ frontend/src/screens/home/home.tsx
@@ -3,8 +3,10 @@ import { View, Button } from 'react-native';
 import { styled } from 'nativewind';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-url-polyfill/auto';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
+import { BASE_URL } from '@env';
+
 
 // interface HomeScreenProps {
 //   // This actually should be `any`, so disabling the linter rule
@@ -20,7 +22,40 @@ const StyledView = styled(View);
 
 // Log out button to test authentication
 const TestLogOut = () => {
-  const { signOut } = useAuth();
+  const { signOut, session } = useAuth();
+  const [developers, setDevelopers] = useState([]);
+
+  // Test function to get token from session
+  const getToken = () => {
+    const token = session.access_token;
+
+    //test getting token
+    console.log(token);
+  
+    return token;
+  };
+
+  // Test endpoint to get all developers from backend
+  const getDevelopers = async () => {
+    console.log(`http://${BASE_URL}/api/v1/developers`);
+    const token = session.access_token;
+    const response = await fetch(`${BASE_URL}/api/v1/developers`, {
+      method: 'GET',
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setDevelopers(data);
+  };
+
+  useEffect(() => {
+    getToken();
+    getDevelopers();
+  }
+  , []);
   
   return (
     <StyledView className="w-[100vw] h-[100vh] flex items-center justify-center">
--- frontend/src/types/env.d.ts
+++ frontend/src/types/env.d.ts
@@ -3,4 +3,5 @@ declare module '@env' {
   export const TS3_SUPABASE_PROJECT_URL: string;
   export const TS3_SUPABASE_JWT_SECRET: string;
   export const TS3_SUPABASE_API_KEY: string;
+  export const BASE_URL: string;
 }
