d69b61f2cf6744f240c5957349f8187014f622f3
gus-s-42 <gusnbastar@gmail.com>
2024-11-05 19:47:30 -0500

    Added comments to Divider

--- frontend/src/components/Divider.tsx
+++ frontend/src/components/Divider.tsx
@@ -6,6 +6,11 @@ import { CaptionText } from './typography';
 const StyledView = styled(View);
 const StyledImage = styled(Image);
 
+/**
+ * Allows us to conditionally render the image or text for the divider.
+ * I believe this is different from CVA b/c the format should be the same every time,
+ * but the rendered components will be different.
+ */
 interface DividerProps {
   text?: string;
   image?: ImageSourcePropType;
@@ -14,18 +19,21 @@ interface DividerProps {
 const Divider: React.FC<DividerProps> = ({ text, image }) => {
   return (
     <StyledView className='relative flex-row items-center w-full'>
-      <StyledView className='flex-1 h-[0.2vh] bg-gray-300'></StyledView>
+      <StyledView className='flex-1 h-[0.2vh] bg-gray-300'/>
       {text && (
+        //Allows for conditional rendering of text element
+        //Text element styling is taken from typography.tsx
         <StyledView className='items-center px-[0.5vh]'>
           <CaptionText>{text}</CaptionText>
         </StyledView>
       )}
       {image && (
+        //Allows for conditional rendering of image element
         <StyledView className='items-center px-[0.5vh]'>
           <StyledImage source={image} className='w-[5vw] h-[5vw]' />
         </StyledView>
       )}
-      <StyledView className='flex-1 h-[0.2vh] bg-gray-300'></StyledView>
+      <StyledView className='flex-1 h-[0.2vh] bg-gray-300'/>
     </StyledView>
   );
 };
