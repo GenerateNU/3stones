f5d471505559a0c893141ed9d82c90de031b8eb0
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2024-11-30 10:13:18 -0500

    Changes to Portfolio Details Component

--- frontend/src/screens/portfolio/components/PortfolioDetails.tsx
+++ frontend/src/screens/portfolio/components/PortfolioDetails.tsx
@@ -6,7 +6,17 @@ const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledImage = styled(Image);
 
-const PortfolioDetails = ()=> {
+interface PortfolioDetailsProps {
+    netPortfolioValue: number;
+    portfolioChangeAmount: number;
+    marketValue: number;
+    cashValue: number;
+    totalProjects: number;
+    expMaturity: number;
+    
+}
+
+const PortfolioDetails = ({netPortfolioValue, portfolioChangeAmount, marketValue, cashValue, totalProjects, expMaturity} : PortfolioDetailsProps & { className?: string })=> {
     return (
         <StyledView className = 'w-full flex flex-col items-start gap-6 self-stretch px-6 pb-8 pt-0'>
             {/* Top Part */}
@@ -20,10 +30,10 @@ const PortfolioDetails = ()=> {
                         ></StyledImage>
                     </StyledView>
                 </StyledView>
-                <StyledText className = 'font-bold text-[32px]'>$12,345.67</StyledText>
+                <StyledText className = 'font-bold text-[32px]'>{netPortfolioValue}</StyledText>
                 <StyledView className = 'flex flex-row justify-between items-center'>
-                    <StyledText className = 'color-success font-sourceSans3Bold text-[16px]'>+ $350.23</StyledText>
-                    <StyledText className = 'color-success px-4 font-sourceSans3Bold text-[16px]'>+ 9.70%</StyledText>
+                    <StyledText className = 'color-success font-sourceSans3Bold text-[16px]'> {portfolioChangeAmount}</StyledText>
+                    <StyledText className = 'color-success px-4 font-sourceSans3Bold text-[16px]'>{portfolioChangeAmount/netPortfolioValue*100}</StyledText>
                     <StyledText className = 'font-bodyBold text-[16px]'>Total Return</StyledText>
                 </StyledView>
             </StyledView>
@@ -32,21 +42,21 @@ const PortfolioDetails = ()=> {
                 <StyledView className = 'flex flex-col gap-6'>
                     <StyledView className = 'flex flex-col items-start gap-1'>
                         <StyledText className = 'font-body text-[16px]'>Market Value</StyledText>
-                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>$10,000.00</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{marketValue}</StyledText>
                     </StyledView>
                         <StyledView className = 'flex flex-col items-start gap-1'>
                         <StyledText className = 'font-body text-[16px]'>Cash Value</StyledText>
-                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>$2,345.67</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{cashValue}</StyledText>
                     </StyledView>
                 </StyledView>
                 <StyledView className = 'flex flex-col gap-6'>
                     <StyledView className = 'flex flex-col items-start gap-1'>
-                        <StyledText className = 'font-body text-[16px]'>Total Positions</StyledText>
-                        <StyledText className = 'font-sourceSans3Bold text-[18px]'> 11</StyledText>
+                        <StyledText className = 'font-body text-[16px]'>Total Projects</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{totalProjects}</StyledText>
                     </StyledView>
                     <StyledView className = 'flex flex-col items-start gap-1'>
                         <StyledText className = 'font-body text-[16px]'>Exp Maturity</StyledText>
-                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>12.5 Years</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{expMaturity}</StyledText>
                     </StyledView>
                 </StyledView>
             </StyledView>
