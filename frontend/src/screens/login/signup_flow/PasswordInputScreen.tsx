c647dbe449c38c9967a45b85b8537648f27c9229
Melody Yu <118621363+melodyyu754@users.noreply.github.com>
2024-12-02 20:10:24 -0500

    Added GOOGLE_PLACES_API to app.config.js (converted from app.json), m… (#77)
    Co-authored-by: Ryan Saperstein <saperstein.r@northeastern.edu>

--- frontend/src/screens/login/signup_flow/PasswordInputScreen.tsx
+++ frontend/src/screens/login/signup_flow/PasswordInputScreen.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, KeyboardAvoidingView, Platform, ScrollView, TextInput } from 'react-native';
+import { View, Text } from 'react-native';
 import { styled } from 'nativewind';
 import Button from '../../../components/Button';
-import ProgressBar from '../../../components/ProgressBar';
 import TextInputComponent from '../components/TextInputComponent';
 import { useAuth } from '../../../context/AuthContext';
+import NavProgressBar from '../components/NavProgressBar';
+import OnboardingScreenWrapper from '../components/OnboardingScreenWrapper';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
-const StyledKeyboardAvoidingView = styled(KeyboardAvoidingView);
-const StyledScrollView = styled(ScrollView);
 
 export default function PasswordInputScreen({ navigation }) {
   const { signupData, updateSignupData } = useAuth();
@@ -21,49 +20,35 @@ export default function PasswordInputScreen({ navigation }) {
   };
 
   return (
-    <StyledKeyboardAvoidingView
-      className='flex-1'
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={80}
-    >
-      <StyledScrollView className='flex-1' contentContainerStyle={{ flexGrow: 1 }}>
-        <StyledView className='flex-1 items-center bg-surfaceBG p-6 justify-between'>
-          {/* Progress Bar */}
-          <StyledView className='w-full mb-4'>
-            <ProgressBar current={2} total={6} />
-          </StyledView>
+    <OnboardingScreenWrapper>
+      <NavProgressBar currentStep={2} totalSteps={6} buttonType={'back'} onPress={() => navigation.goBack()} />
 
-          {/* Password Input Section */}
-          <StyledView className='w-full flex-1 justify-center items-center'>
-            <StyledText className='text-center text-2xl font-bold text-black mb-2'>
-              Create a password
-            </StyledText>
-            <StyledText className='text-center text-gray-600 mb-8'>
-              Choose a strong password for your account.
-            </StyledText>
 
-            <TextInputComponent
-              placeholder='Password'
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              isPassword={true}
-            ></TextInputComponent>
-          </StyledView>
+      {/* Password Input Section */}
+      <StyledView className="w-full flex-1 justify-center items-center">
+        <StyledText className="text-center text-2xl font-bold text-black mb-2">Create a password</StyledText>
+        <StyledText className="text-center text-gray-600 mb-8">
+          Choose a strong password for your account.
+        </StyledText>
 
-          {/* Continue Button */}
-          <StyledView className='w-full mt-6'>
-            <Button
-              type='primary'
-              onPress={() => {
-                handleNext();
-              }}
-              disabled={!password.trim()} // Disable if password is empty
-            >
-              Continue
-            </Button>
-          </StyledView>
-        </StyledView>
-      </StyledScrollView>
-    </StyledKeyboardAvoidingView>
+        <TextInputComponent
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          isPassword={true}
+        >
+        </TextInputComponent>
+      </StyledView>
+
+      {/* Continue Button */}
+      <StyledView className="w-full mt-6">
+        <Button
+          type="primary"
+          onPress={() => { handleNext() }}
+          disabled={!password.trim()} // Disable if password is empty
+        >Continue</Button>
+      </StyledView>
+
+    </OnboardingScreenWrapper>
   );
 }
