7df43943aaae7746b51cd9639fa003d4817c347f
Michael Brennan <michael.brennancolorado@gmail.com>
2024-12-05 14:12:44 -0500

    beginning fixes

--- frontend/src/screens/portfolio/components/PortfolioDetails.tsx
+++ frontend/src/screens/portfolio/components/PortfolioDetails.tsx
@@ -1,65 +1,87 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { styled } from 'nativewind';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledImage = styled(Image);
+const StyledTouchableOpacity = styled(TouchableOpacity);
 
-const PortfolioDetails = () => {
-  return (
-    <StyledView className='w-full flex flex-col items-start gap-6 self-stretch px-6 pb-8 pt-0'>
-      {/* Top Part */}
-      <StyledView className='flex flex-col items-start gap-1 self-stretch'>
-        <StyledView className='flex flex-row'>
-          <StyledText>Net Portfolio Value</StyledText>
-          <StyledView className='px-2'>
-            <StyledImage
-              source={require('../../../../assets/images/remove_red_eye.png')}
-              className='w-[5vw] h-[5vw]'
-            ></StyledImage>
-          </StyledView>
-        </StyledView>
-        <StyledText className='font-bold'>$12,345.67</StyledText>
-        <StyledView className='flex flex-row justify-between items-center'>
-          <StyledText className='color-success'>$350.23</StyledText>
-          <StyledText className='color-success px-4'>+ 9.70%</StyledText>
-          <StyledText>Total Return</StyledText>
-        </StyledView>
-      </StyledView>
-      {/* Four grid */}
-      <StyledView className='flex flex-col p-4 w-full items-center'>
-        <StyledView className='flex-row gap-5 p-4'>
-          <StyledView className='flex justify-center w-1/2'>
-            <StyledView className='flex flex-col items-start gap-1'>
-              <StyledText> Market Value</StyledText>
-              <StyledText> $10,000.00</StyledText>
+interface PortfolioDetailsProps {
+    netPortfolioValue: number;
+    portfolioChangeAmount: number;
+    marketValue: number;
+    cashValue: number;
+    totalProjects: number;
+    expMaturity: number;
+    
+}
+
+const PortfolioDetails = ({netPortfolioValue, portfolioChangeAmount, marketValue, cashValue, totalProjects, expMaturity} : PortfolioDetailsProps & { className?: string })=> {
+
+    const [hideValues, setHideValues] = useState(false);
+
+    
+    const displayValue = (value) => {
+        return hideValues ? '****' : value;
+    };
+
+    const [isEyeOpen, setIsEyeOpen] = useState(true);
+
+    const toggleHideValues = () => {
+        setHideValues(!hideValues);
+        setIsEyeOpen(!isEyeOpen);
+    };
+
+
+    return (
+        <StyledView className = 'w-full flex flex-col items-start gap-6 self-stretch px-6 pb-8 pt-0'>
+            {/* Top Part */}
+            <StyledView className = 'flex flex-col items-start gap-1 self-stretch'>
+                <StyledView className = 'flex flex-row'> 
+                    <StyledText className = 'font-body text-[16px]'>Net Portfolio Value</StyledText>
+                    <StyledTouchableOpacity onPress={toggleHideValues} className = 'px-2'>
+                        <StyledImage
+                            source={isEyeOpen 
+                                ? require('../../../../assets/images/remove_red_eye.png')
+                                : require('../../../../assets/images/View-Icon.png')}
+                            className='w-[5vw] h-[5vw]'
+                        />
+                    </StyledTouchableOpacity>
+                </StyledView>
+                <StyledText className = 'font-bold text-[32px]'>{netPortfolioValue}</StyledText>
+                <StyledView className = 'flex flex-row justify-between items-center'>
+                    <StyledText className = {`${portfolioChangeAmount >= 0 ? 'color-success' : 'color-error'} font-sourceSans3Bold text-[16px]`}> {portfolioChangeAmount}</StyledText>
+                    <StyledText className = {`${portfolioChangeAmount >= 0 ? 'color-success' : 'color-error'} px-4 font-sourceSans3Bold text-[16px]`}>{displayValue((portfolioChangeAmount/netPortfolioValue*100).toFixed(2) + '%')}</StyledText>
+                    <StyledText className = 'font-bodyBold text-[16px]'>Total Return</StyledText>
+                </StyledView>
             </StyledView>
-          </StyledView>
-          <StyledView className='flex justify-center w-1/2 items-end'>
-            <StyledView className='flex flex-col items-start gap-1'>
-              <StyledText> Cash Value</StyledText>
-              <StyledText> $2,345.67</StyledText>
+            {/* Four grid */}
+            <StyledView className = 'w-full flex-row justify-between'> 
+                <StyledView className = 'flex flex-col gap-6'>
+                    <StyledView className = 'flex flex-col items-start gap-1'>
+                        <StyledText className = 'font-body text-[16px]'>Market Value</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{marketValue}</StyledText>
+                    </StyledView>
+                        <StyledView className = 'flex flex-col items-start gap-1'>
+                        <StyledText className = 'font-body text-[16px]'>Cash Value</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{cashValue}</StyledText>
+                    </StyledView>
+                </StyledView>
+                <StyledView className = 'flex flex-col gap-6'>
+                    <StyledView className = 'flex flex-col items-start gap-1'>
+                        <StyledText className = 'font-body text-[16px]'>Total Projects</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{totalProjects}</StyledText>
+                    </StyledView>
+                    <StyledView className = 'flex flex-col items-start gap-1'>
+                        <StyledText className = 'font-body text-[16px]'>Exp Maturity</StyledText>
+                        <StyledText className = 'font-sourceSans3Bold text-[18px]'>{expMaturity}</StyledText>
+                    </StyledView>
+                </StyledView>
             </StyledView>
-          </StyledView>
         </StyledView>
-        <StyledView className='flex-row gap-5 p-4 '>
-          <StyledView className='flex justify-center w-1/2'>
-            <StyledView className='flex flex-col items-start gap-1'>
-              <StyledText> Total Positions</StyledText>
-              <StyledText> 11</StyledText>
-            </StyledView>
-          </StyledView>
-          <StyledView className='flex justify-center w-1/2 items-end'>
-            <StyledView className='flex flex-col items-start gap-1'>
-              <StyledText> Exp Maturity</StyledText>
-              <StyledText> 12.5 Years</StyledText>
-            </StyledView>
-          </StyledView>
-        </StyledView>
-      </StyledView>
-    </StyledView>
-  );
+
+    );
 };
 
 export default PortfolioDetails;
